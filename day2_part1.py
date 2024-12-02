985d06037eb985ab8ee21b87af4d22f542354590
Moritz Grissemann <moritz@luxner-zt.com>
2024-12-02 12:11:00 +0100

    day 2

---
+++ day2_part1.py
@@ -0,0 +1,21 @@
+def isSave(li): # returns -1 if true else index of wrong element
+    mode = li[0] < li[1]  #false = desc
+    for i in range(0,len(li)-1):
+        if (li[i] > li[i+1] and mode is True) or (li[i] < li[i+1] and mode is False):
+            return False
+        distanz = li[i+1] - li[i]
+        if  distanz not in range(-3,4) or distanz == 0:
+            return False
+    return True
+
+f = open("C:\\Users\\mogri\\PycharmProjects\\AoC\\input.txt","r")
+sum = 0
+for line in f:
+    li = line.split()
+    intLi = []
+    for l in li:
+        l.strip()
+        intLi.append(int(l))
+    if(isSave(intLi)):
+        sum +=1
+print(sum)
---
+++ day2_part2.py
@@ -0,0 +1,33 @@
+def isSave(li): # returns -1 if true else index of wrong element
+    mode = li[0] < li[1]  #false = desc
+    for i in range(0,len(li)-1):
+        if (li[i] > li[i+1] and mode is True) or (li[i] < li[i+1] and mode is False):
+            return False
+        distanz = li[i+1] - li[i]
+        if  distanz not in range(-3,4) or distanz == 0:
+            return False
+    return True
+
+def checkLine(li):
+    tempL = li.copy()
+    if isSave(li):
+        return True
+    else:
+        for i in range(0,len(li)):
+            tempL.pop(i)
+            if isSave(tempL):
+                return True
+            tempL = li.copy()
+    return False
+
+f = open("C:\\Users\\mogri\\PycharmProjects\\AoC\\input.txt","r")
+sum = 0
+for line in f:
+    li = line.split()
+    intLi = []
+    for l in li:
+        l.strip()
+        intLi.append(int(l))
+    if checkLine(intLi):
+        sum +=1
+print(sum)
