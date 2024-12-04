839a1707d7513e65d007a41bff2c0a7d03685665
Moritz Grissemann <moritz@luxner-zt.com>
2024-12-04 15:12:24 +0100

    day4

---
+++ day4_part1.py
@@ -0,0 +1,51 @@
+def findCombinations(lines,lineCnt):
+    line = lines[lineCnt]
+    cnt = 0
+    for i in range(0,len(line)): #loops horizontal
+        hor = []
+        ver = []
+        dia = [[]] ##dia[0] -> unten rechts, dia[1] -> unten links
+        dia.append([])
+        for j in range(0,4):   ## extends 4 forwards
+            if i<= len(line)-4:
+                hor.append(line[i+j])
+            if lineCnt + 4 <=len(lines):
+                ver.append(lines[lineCnt+j][i])
+            if i <= len(line)-4 and lineCnt + 4 <=len(lines):
+                dia[0].append(lines[lineCnt+j][i+j])
+            if lineCnt + 4 <=len(lines) and i >= 3:
+                dia[1].append(lines[lineCnt+j][i-j])
+        if containsXmas(hor):
+            print(lineCnt," hor line:", i)
+            cnt +=1
+        if containsXmas(ver):
+            print(lineCnt," ver line:", i)
+            cnt +=1
+        if containsXmas(dia[0]):
+            print(lineCnt," dia rechts line:", i)
+            cnt +=1
+        if containsXmas(dia[1]):
+            print(lineCnt,"dia link line:", i)
+            cnt +=1
+    return cnt
+
+def containsXmas(list):
+
+    s = ''.join(list)
+    if not s.find("XMAS") == -1:
+        return True
+    s = ''.join(reversed(list))
+    if not s.find("XMAS") == -1:
+        return True
+    return False
+
+
+f = open("C:\\Users\\mogri\\PycharmProjects\\AoC\\input.txt","r")
+lines= []
+sum = 0
+for l in f:
+    lines.append(l)
+for i in range(0,len(lines)):
+    sum += findCombinations(lines,i)
+
+print(sum)
---
+++ day4_part2.py
@@ -0,0 +1,49 @@
+
+
+def containsMs(list):
+
+    s = ''.join(list)
+    if not s.find("MS") == -1:
+        return True
+    s = ''.join(reversed(list))
+    if not s.find("MS") == -1:
+        return True
+    return False
+
+def getCords(line, lineCount,lines):
+    index = 1
+    sum = 0
+
+    while(line.find("A",index,len(line)-2) != -1):
+        x = line.find("A",index,len(line)-2)
+        index = x+1
+        y = lineCount
+        print(x,y)
+        if correct(x,y,lines):
+            sum +=1
+    return sum
+def correct(x,y,lines):
+    pos1 = [(-1,-1),(1,1)]
+    pos2 = [(1,-1),(-1,1)]
+    l1 = []
+    for pos in pos1:
+        l1.append(lines[y+pos[1]][x+ pos[0]]) ## pos[1] = y, pos[0] = x
+    if not containsMs(l1):
+        return False
+    l2 = []
+    for pos in pos2:
+        l2.append(lines[y+pos[1]][x + pos[0]]) ## pos[1] = y, pos[0] = x
+    if not containsMs(l2):
+        return False
+    return True
+
+
+f = open("C:\\Users\\mogri\\PycharmProjects\\AoC\\input.txt","r")
+lines= []
+sum = 0
+for l in f:
+    lines.append(l)
+for i in range(1,len(lines)-1):
+    sum += getCords(lines[i], i, lines)
+
+print(sum)
