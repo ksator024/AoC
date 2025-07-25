66e74ffc8e7781dec122920d91c050583a2f779f
Moritz <moritz@luxner-zt.com>
2025-07-25 14:54:28 +0200

    Day 8

--- AoC_2022/Day8_part2.py
+++ AoC_2022/Day8_part2.py
@@ -1,10 +1,11 @@
 def checkSubstring(subString, max):
-
+    count = 0
     for tree in subString:
+        count += 1
+        print(subString,max,tree)
         if tree >= max:
-            return False
-        #print("true",max, subString)
-    return True
+            return count
+    return count
 
 f = open("../Input.txt", "r")
 trees = []
@@ -16,24 +17,31 @@ for line in f:
             trees[-1].append(int(l))
 
 print(trees)
-sum = 0
+score = []
+index = 0
 for i in range(0,len(trees)):
     for j in range(0,len(trees[i])):
+        count = 1
+        index += 1
         tree = trees[i][j]
         y = []
         for k in range(0,len(trees)):
            y.append(trees[k][j])
         pos = []
-        y1 = y[:i]
+        y1 = (y[:i])
+        y1.reverse()
         y2 = y[i+1:]
         x1 = trees[i][j+1:]
         x2 = trees[i][:j]
+        x2.reverse()
         pos.extend([x2,x1,y1,y2])
         for p in pos:
             #print("zahl", p)
-            if checkSubstring(p, tree):
-                print(tree)
-                sum += 1
-                break
+            temp = checkSubstring(p,tree)
+            count *= temp
+            print(temp, index)
+        print("new", count)
+        score.append(count)
 
-print(sum)
+print(max(score))
+print(score)
